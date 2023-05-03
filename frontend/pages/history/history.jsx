0e01f3862f0740520e93a40d07086723a54f7a40
KoruJuriSan <mr.collejoulian@gmail.com>
2023-05-03 17:37:18 +0200

    adding history page

--- frontend/pages/history/history.jsx
+++ frontend/pages/history/history.jsx
@@ -2,6 +2,12 @@ const React = require("react")
 
 module.exports = function History() {
     return (
-        <h1 className=" text-3xl text-center pt-10 font-bold">COMMING SOON !</h1>
+        <div className=" flex w-full items-center flex-col">
+            <h1 className=" text-3xl text-center pt-24 font-bold">Historique du projet</h1>
+            <p className=" max-w-xl pt-5">
+                Notre équipe est composé de Joulian, Brendan et Romain. Ce projet à été réalisé dans le cadre de notre projet de fin
+                d'étude et le concepte de celui-ci nous à été proposé par notre proffesseur de réseau <italic>Patrick Hainaut</italic>
+            </p>
+        </div>
     )
 }
