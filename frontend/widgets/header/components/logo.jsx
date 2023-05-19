0e42928f1f77dc905cf9cf8e57970af73f09adf8
KoruJuriSan <mr.collejoulian@gmail.com>
2023-05-19 17:00:50 +0200

    update versions emplacement

--- frontend/widgets/header/components/logo.jsx
+++ frontend/widgets/header/components/logo.jsx
@@ -4,7 +4,7 @@ const { NavLink } = require("react-router-dom")
 module.exports = function Logo() {
     return (
         <NavLink to={"/"} reloadDocument>
-            <span className=" text-2xl bold text-white">{"sub.MASK >"}</span>
+            <span className=" text-2xl bold text-white">{"sub.MASK >"}<span className=" text-2xl">1.3.2</span></span>
         </NavLink>
     )
 }
