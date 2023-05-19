8f457979570c3074c37c5127ec26bb1cba4ecc55
KoruJuriSan <mr.collejoulian@gmail.com>
2023-05-19 17:10:40 +0200

    Correction de fautes d'orthographe

--- frontend/pages/history/history.jsx
+++ frontend/pages/history/history.jsx
@@ -5,8 +5,8 @@ module.exports = function History() {
         <div className=" flex w-full items-center flex-col">
             <h1 className=" text-3xl text-center pt-24 font-bold">Historique du projet</h1>
             <p className=" max-w-xl pt-5">
-                Notre équipe est composé de Joulian, Brendan et Romain. Ce projet à été réalisé dans le cadre de notre projet de fin
-                d'étude et le concepte de celui-ci nous à été proposé par notre proffesseur de réseau <italic>Patrick Hainaut</italic>
+                Notre équipe est composée de Joulian, Brendan et Romain. Ce projet a été réalisé dans le cadre de notre projet de
+                fin d&rsquo;études et le concept de celui-ci nous a été proposé par notre professeur de réseau <italic>Patrick Hainaut</italic>.
             </p>
         </div>
     )
